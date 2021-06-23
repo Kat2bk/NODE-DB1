73b36a74d9a5fec02d115dcaeff39943e56c2a40
unknown <Katherine2bk@gmail.com>
2021-06-23 09:50:53 -0700

    finished first two SQL queries from the queries.sql file

--- queries.sql
+++ queries.sql
@@ -1,10 +1,13 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers WHERE PostalCode is '1010';
 
 -- Find the phone number for the supplier with the id 11
+SELECT Phone FROM Suppliers WHERE SupplierID = '11';
 
 -- List first 10 orders placed, sorted descending by the order date
+
 
 -- Find all customers that live in London, Madrid, or Brazil
 
@@ -15,3 +18,4 @@
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
