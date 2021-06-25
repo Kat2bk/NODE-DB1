8a785ac244666232e899270d000489d12ad8b9ac
unknown <Katherine2bk@gmail.com>
2021-06-25 11:44:45 -0700

    imported db.config inside accounts-model.js... set up get request for home, server works

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,9 +1,12 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // grabs an array of accounts
+  return db.select().table('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  
 }
 
 const create = account => {
--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
 const server = express();
-
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Welcome to Accounts API');
+})
+
+server.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.json({status: 500, message: 'Something happened, try again'})
+})
 
 module.exports = server;
